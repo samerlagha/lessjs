81c3e1003f010816f02f064fb7501ccfbcce1f0e
samerlagha <samer_la@mail.ru>
2019-10-30 12:18:04 +0200

    updat file

--- lesson3/dz2/index.js
+++ lesson3/dz2/index.js
@@ -10,7 +10,7 @@ console.log("true to number is " + Number(true));
 console.log("false to number is " + Number(false));
 console.log("null to number is " + Number(null));
 console.log("undefined to number is " + Number(undefined));
-console.log("' 20 ' to number is " + Number(' 20 '));
+console.log("'   20   ' to number is " + Number('   20   '));
 console.log("'      ' to number is " + Number('      '));
 console.log("'   30d   ' to number is " + Number('   30d   '));
 

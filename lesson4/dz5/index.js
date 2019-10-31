0b53663e104642273306783043f23c8932167a1f
samerlagha <samer_la@mail.ru>
2019-10-31 15:24:25 +0200

    init file

---
+++ lesson4/dz5/index.js
@@ -0,0 +1,14 @@
+const m =10;
+const n =20;
+let result =0;
+    
+while(true){
+if(m % 2===0 && m % 4 !==0) result += m;
+     
+if(m % 4===0) result *=m;
+if(m % 3===0) result -=m;
+if(m % 5===0) result =m;
+
+    break;
+    }
+console.log('Result: ' + result);

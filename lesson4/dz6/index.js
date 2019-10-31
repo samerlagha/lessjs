79d7dc3712f10fba6c9fd89ccd8b6c85d7584add
samerlagha <samer_la@mail.ru>
2019-10-31 16:53:41 +0200

    update file

--- lesson4/dz6/index.js
+++ lesson4/dz6/index.js
@@ -1,9 +1,22 @@
-for(let i=0;i<=100;i++){
+// for(let i=0;i<=100;i++){
+//     if(i%2 !==0){
+//         console.log(' Found ');
+//         sum+=i;
+//         if(i*5 >500) console.log(' Bigger');
+//         else console.log('Smaller or equal');
+//     }
+
+// }
+let count=0;
+for(let i=0;i<=1000;i++){
     if(i%2 !==0){
+         
         console.log(' Found ');
-        sum+=i;
-        if(i*5 >500) console.log(' Bigger');
+        count++;
+         console.log(count);
+        //sum+=i;
+        if(count*5 >500) console.log('Bigger');
         else console.log('Smaller or equal');
     }
-
 }
+   

67ab7e107d6c36cf258c9115804cf049490dd96f
samerlagha <samer_la@mail.ru>
2019-10-31 18:25:22 +0200

    update file

--- lesson4/dz3/index.js
+++ lesson4/dz3/index.js
@@ -1,8 +1,7 @@
-for(let i = 1; i <= 5 ; i++){
-    
-    console.log('\n=>  '+ i + '  <=\n\n');
-     for(let j =1 ; j<=5 ; j++){
-         console.log(i + '  X  ' + j + ' = '+ i*j);
-     }
-    
- }
+
+ for (let i = 1; i <= 5; i++) {
+    console.log('=>' + i + '<=');
+    for (let j = 1; j <= 10; j++) {
+        console.log(i + ' x ' + j + ' = ' + (i * j));
+    }
+}

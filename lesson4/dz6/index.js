76b0c2ea7ce8b0fbdabb0196afbf836a34709f43
samerlagha <samer_la@mail.ru>
2019-10-31 16:38:13 +0200

    init file

---
+++ lesson4/dz6/index.js
@@ -0,0 +1,9 @@
+for(let i=0;i<=100;i++){
+    if(i%2 !==0){
+        console.log(' Found ');
+        sum+=i;
+        if(i*5 >500) console.log(' Bigger');
+        else console.log('Smaller or equal');
+    }
+
+}

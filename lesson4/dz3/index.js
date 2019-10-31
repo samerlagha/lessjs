939f2adcf321b77ee7b57152cb65b0c8b27475c5
samerlagha <samer_la@mail.ru>
2019-10-31 12:43:01 +0200

    init file

--- lesson4/dz3/index.js
+++ lesson4/dz3/index.js
@@ -2,6 +2,6 @@ for(let i = 1; i <= 5 ; i++){
     console.log('=> '+ i + ' <=');
 
     for(let j =1 ; j<=5 ; j++){
-        console.log(i + '  X  ' + j + ' = '+ i*j);
+        console.log(i + '  X  ' + j + ' =  '+ i*j);
     }
 }

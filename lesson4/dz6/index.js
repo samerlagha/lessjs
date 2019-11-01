e92bfcc04e80bed1b53ac06be5c95c65ba36ccb5
samerlagha <samer_la@mail.ru>
2019-11-01 10:18:15 +0200

    update file

--- lesson4/dz6/index.js
+++ lesson4/dz6/index.js
@@ -7,17 +7,32 @@
 //     }
 
 // }
+// let count=0;
+// for(let i=0;i<=1000;i++){
+//     if(i%2 !==0){
+         
+//         console.log(' Found ');
+//         count++;
+//          count +=count;
+//          console.log(count);
+//         //sum+=i;
+//         if(count*5 >500) console.log('Bigger');
+//         else console.log('Smaller or equal');
+//     }
+// }
+
 let count=0;
 for(let i=0;i<=1000;i++){
     if(i%2 !==0){
          
         console.log(' Found ');
         count++;
-         count +=count;
-         console.log(count);
-        //sum+=i;
-        if(count*5 >500) console.log('Bigger');
+          console.log(count);
+        }
+       
+        if(count*5 >5000) console.log('Bigger');
         else console.log('Smaller or equal');
     }
-}
+
+
    

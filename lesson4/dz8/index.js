052ffbc16f0d91169e664e095c1960406774c778
samerlagha <samer_la@mail.ru>
2019-11-01 12:37:56 +0200

    update file

--- lesson4/dz8/index.js
+++ lesson4/dz8/index.js
@@ -1,4 +1,8 @@
-for(let i=0; i<=1000;i++){
-
-
+let count=0;
+for(let i=1; i<=1000;i++){ 
+count++;
 }
+ let res1=parseInt(count/1234);
+ let  res2 = count%1234;
+
+ res1 > res2 ? console.log(true) : console.log(false);

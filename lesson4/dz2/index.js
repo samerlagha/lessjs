85e9f303606bd9950e67834924da662e80ca636a
samerlagha <samer_la@mail.ru>
2019-11-01 09:08:28 +0200

    Update index.js

--- lesson4/dz2/index.js
+++ lesson4/dz2/index.js
@@ -1,8 +1,6 @@
 const m = 10;
 const n = 20;
 let result = 1;
-
-<<<<<<< HEAD
 for (let i = m; i <= n; i++){
     if (!(i % 2)) continue;
     console.log(result + ' * ' + i + ' = ' + (result *= i));

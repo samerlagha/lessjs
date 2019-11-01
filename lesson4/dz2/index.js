19df84039a538e90fd2955c54f69ef75492e5c61
samerlagha <samer_la@mail.ru>
2019-11-01 09:05:54 +0200

    update file

--- lesson4/dz2/index.js
+++ lesson4/dz2/index.js
@@ -1,20 +1,8 @@
 const m = 10;
 const n = 20;
-let  result = 1;
+let result = 1;
 
-do{
-   if (!(i % 2)) continue;
-     if(m % 2 === 1){
-        result *=m;
-     }
-     m++;
-} while(m <= n);
-console.log('Result: '+ result);
-// const m = 10;
-// const n = 20;
-// let result = 1;
-
-// for (let i = m; i <= n; i++){
-//     if (!(i % 2)) continue;
-//     console.log(result + ' * ' + i + ' = ' + (result *= i));
-// }
+for (let i = m; i <= n; i++){
+    if (!(i % 2)) continue;
+    console.log(result + ' * ' + i + ' = ' + (result *= i));
+}

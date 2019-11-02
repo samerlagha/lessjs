eb674e0950437b9dd75ad1e6435e7c99f6fb32ac
samerlagha <samer_la@mail.ru>
2019-11-02 17:04:00 +0200

    update

--- lesson5/dz7/index.js
+++ lesson5/dz7/index.js
@@ -2,11 +2,7 @@ function  increaser(a, index){
   let  roz = index -a;
    if(a <=  index){
      roz = a+roz;
-   
-     //return roz;
-   } else if(a > index) {
-           roz= a;
-       //  return a;
-     }
      return roz;
+   } else if(a > index)   return a;
+    
 }

e1c955b11e9395f3eda26ad0e808bc9e7268633e
samerlagha <samer_la@mail.ru>
2019-11-02 19:35:05 +0200

    update

--- lesson6/dz3/index.js
+++ lesson6/dz3/index.js
@@ -1,6 +1,6 @@
-function swap(from,to){
+function getSpecialNumbers(m,n){
  let specialNumbers=[];
- for(let i = from; i <= to;i++){
+ for(let i = m; i <= n;i++){
 
     if(i % 3 ===0){
         specialNumbers.push(i);

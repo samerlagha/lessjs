2e6d79ba71244131f0c806afee176cbe3e1e7f1c
samerlagha <samer_la@mail.ru>
2019-11-02 12:56:14 +0200

    init file

--- lesson5/dz7/index.js
+++ lesson5/dz7/index.js
@@ -1,4 +1,10 @@
-function increaser(a,index){
-
-    
-}
+function  increaser(a, index){
+    let  roz = index -a;
+     if(a <= index){
+       a = a+roz;
+       return a;
+     } else return a;
+ }
+ 
+  let res =increaser(6,5);
+  console.log(res);

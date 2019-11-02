8ab2c9af0261122d82aec69e0fa8ee0249d38c5d
samerlagha <samer_la@mail.ru>
2019-11-02 16:43:44 +0200

    update file

--- lesson5/dz9/index.js
+++ lesson5/dz9/index.js
@@ -2,7 +2,7 @@ function findDivCount(a, b, n){
     let res =0;
     for (let i = a; i < b; i++) {
               if (i % n == 0){
-                  res +=i;
+                  res ++;
               } else if(i % n ==0){
                   return 0;
               }

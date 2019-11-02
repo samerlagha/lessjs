97f3dea42598086c9f4e74421d9e98a532a08135
samerlagha <samer_la@mail.ru>
2019-11-02 08:32:39 +0200

    init file

--- lesson5/dz5/index.js
+++ lesson5/dz5/index.js
@@ -1,16 +1,12 @@
 function getSum(num1,num2){
-
-    let result=0;
- for ( num1=0; num1 <= num2;num1++){
-  if (num1 % 2 !==0 ) continue;
-  // sum= num1+num2;
-
-  // return sum;
- 
- console.log(result + ' + ' + num1 + ' = ' + (result += num1));
+    let sum=0;
+    for (let i=num1 ; i <= num2; i++) {
+   if (i % 2 === 0)  sum +=i;
+   
+  }
+  console.log(sum); 
 }
-}
-let resut=  getSum(0,0);
+getSum(4,20);
 
 // function getSum(num1,num2){
 //     if(num1 %2===0 && num2 % 2 ===0){

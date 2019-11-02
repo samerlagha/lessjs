67f0cbf7457e91031486bb0545048603f0452f88
samerlagha <samer_la@mail.ru>
2019-11-02 13:33:31 +0200

    update

--- lesson5/dz7/index.js
+++ lesson5/dz7/index.js
@@ -1,13 +1,14 @@
 function  increaser(a, index){
-    let  roz = index -a;
+    //let  roz = index -a;
      if(parseInt(a) <=  parseInt(index)){
-       a = a+roz;
-       console.log(a);
+       //a = a+roz;
+       a = index;
+       //console.log(a);
        return a;
      } else if(a > index) {
-        console.log(a);
+       // console.log(a);
            return a;
        }
  }
  
-  increaser(7,5);
+ // increaser(7,5);

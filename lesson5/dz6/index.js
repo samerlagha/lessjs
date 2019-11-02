cd1efe9b8d02605515bdc1dcca639cfaaf77c059
samerlagha <samer_la@mail.ru>
2019-11-02 11:28:09 +0200

    update file

--- lesson5/dz6/index.js
+++ lesson5/dz6/index.js
@@ -1,16 +1,20 @@
-function getPrimes(num1,num2){
-    nextPrime:
-  for( let i =num1; i <=num2;i++){
-   // if(i <1) continue;
-   if(i>0){
-    for (let j = 2; j < i; j++) { 
-        if (i % j == 0) continue nextPrime; 
-      }
-      console.log(i);
-  }
+function  getPrimes(num1,num2){
+    for (let i = 1; i <= num2; i++)
+    {
+        let not_prime = false;
+    
+        for (let j = 2; j <= (i - 1); j++) 
+    
+        {
+            if (i % j === 0) 
+            {not_prime = true;}
+        }
+    
+        if (not_prime === false) 
+        {console.log(i);}
     }
-  }
-getPrimes(2,10);
+    }
+    getPrimes(2,10);
 
 
 

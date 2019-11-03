8ec47160120a0ae1a93069887a2e43e636d8177d
samerlagha <samer_la@mail.ru>
2019-11-03 13:15:32 +0200

    update

--- lesson6/dz7/index.js
+++ lesson6/dz7/index.js
@@ -1,4 +1,18 @@
-function sortAsc(array){
+function sortAsc(array) {
+    for (let i = array.length-1; i >= 0; i--) {
+      for (let j = i; j >= 0; j--) {
+        if (array[i] < array[j]) {
+          let temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+      }
+    }
+    return array;
+  }
+
+
+function sortDesc(array){
 
     for (let i = 0; i < array.length; i++) {
       
@@ -13,15 +27,3 @@ function sortAsc(array){
     return array;
   }
  
-  function sortDesc(array) {
-    for (let i = array.length-1; i >= 0; i--) {
-      for (let j = i; j >= 0; j--) {
-        if (array[i] < array[j]) {
-          let temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-        }
-      }
-    }
-    return array;
-  }

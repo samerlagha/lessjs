55aef7e20d02379df4499c794965e8d882951743
samerlagha <samer_la@mail.ru>
2019-11-03 10:45:32 +0200

    init

--- lesson6/dz5/index.js
+++ lesson6/dz5/index.js
@@ -2,7 +2,7 @@ function squareArray(arr){
     
     ret= [];
  
-     if(!Array.isArray(arr)) return 0;
+     if(!Array.isArray(arr)) return null;
  
      for (let i = 0 ; i = arr.length; i++) {
          ret.push(arr[i] * arr[i]);
--- lesson6/dz6/index.js
+++ lesson6/dz6/index.js
@@ -1,6 +1,6 @@
 function reverseArray(arr){
     let arr2=[]
-  if(!(Array.isArray(arr))) return 0;
+  if(!(Array.isArray(arr))) return null;
   
     arr2= arr.reverse();
     return arr2;

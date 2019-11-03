7a4f84349dd30f2f78ad61615cc425c4f2188a87
samerlagha <samer_la@mail.ru>
2019-11-03 12:01:45 +0200

    init file

---
+++ lesson6/dz7/index.js
@@ -0,0 +1,45 @@
+ function sortAsc(array){
+
+ let array=[];
+ for (i=array.length-1;i>=0;i--) {
+           
+for (j=0;j<i;j++) {
+    if (array[j]>array[j+1]) {
+        let temp=array[j];
+            array[j]=array[j+1];
+             array[j+1]=temp;
+                }
+            }
+        }   
+
+        console.log(array);
+ }
+
+ function sortDesc(array){
+
+    let n = array.length;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - 1 - i; j++) {
+        if (array[j + 1] < array[j]) {
+          let t = array[j + 1];
+          array[j + 1] = array[j];
+          array[j] = t;
+        }
+      }
+    }
+    return array; 
+  }
+function sortDesc(array){
+
+    let n = array.length;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - 1 - i; j++) {
+        if (array[j + 1] < array[j]) {
+          let t = array[j + 1];
+          array[j + 1] = array[j];
+          array[j] = t;
+        }
+      }
+    }
+    return array; 
+  }

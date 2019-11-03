a590534b61d8b8957ab2ef9b07828494d424cc76
samerlagha <samer_la@mail.ru>
2019-11-03 12:04:47 +0200

    update

--- lesson6/dz7/index.js
+++ lesson6/dz7/index.js
@@ -15,20 +15,6 @@ for (j=0;j<i;j++) {
         console.log(array);
  }
 
- function sortDesc(array){
-
-    let n = array.length;
-    for (let i = 0; i < n - 1; i++) {
-      for (let j = 0; j < n - 1 - i; j++) {
-        if (array[j + 1] < array[j]) {
-          let t = array[j + 1];
-          array[j + 1] = array[j];
-          array[j] = t;
-        }
-      }
-    }
-    return array; 
-  }
 function sortDesc(array){
 
     let n = array.length;

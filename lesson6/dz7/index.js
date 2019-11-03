fa210c34d88d67abed2603b1306b21ee55fad076
samerlagha <samer_la@mail.ru>
2019-11-03 12:28:28 +0200

    ini

--- lesson6/dz7/index.js
+++ lesson6/dz7/index.js
@@ -1,16 +1,16 @@
  function sortAsc(array){
 
-//  let array=[];
-//  for (i=array.length-1;i>=0;i--) {
+ let array=[];
+ for (i=array.length-1;i>=0;i--) {
            
-// for (j=0;j<i;j++) {
-//     if (array[j]>array[j+1]) {
-//         let temp=array[j];
-//             array[j]=array[j+1];
-//              array[j+1]=temp;
-//                 }
-//             }
-//         }   
+for (j=0;j<i;j++) {
+    if (array[j]>array[j+1]) {
+        let temp=array[j];
+            array[j]=array[j+1];
+             array[j+1]=temp;
+                }
+            }
+        }   
 
        // console.log(array);
  }

83c1a42e3446528976c9c7dc1d5dc430c0d30e6c
samerlagha <samer_la@mail.ru>
2019-11-03 15:40:18 +0200

    update

--- lesson6/dz12/index.js
+++ lesson6/dz12/index.js
@@ -1,6 +1,15 @@
 function cloneArr(arr){
-    let temp = { arr : arr };
- let obj = $.extend(true, {}, temp);
- return obj.arr;
- 
- }
+    if (!arr) {
+       return arr;
+     }
+   
+     let v;
+     let bObject = Array.isArray(arr) ? [] : {};
+     for (const k in arr) {
+       v = arr[k];
+       bObject[k] = (typeof v === "object") ? copy(v) : v;
+     }
+   
+     return bObject;
+   
+   }

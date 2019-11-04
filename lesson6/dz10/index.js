af95fb39317c88352de6a5ccee78f8c1ddf6998f
samerlagha <samer_la@mail.ru>
2019-11-04 11:42:32 +0200

    update

--- lesson6/dz10/index.js
+++ lesson6/dz10/index.js
@@ -1,16 +1,12 @@
-function withdraw(clients, balances, client, amount){
-    client=[];
-  // amount =[];
-  if(amount > balances) return -1;
-   for(let i =0 ; i <= clients.length-1;i++){
-    for(let j=0 ; j <= balances.length-1;j++){
-            if(balances[i]< amount) {return -1}
-             else if (balances[i]> amount){
-               balances[j] -=amount;}
-             
-    }
-     return balances;
-   }
-  
-    return clients;
-   }
+function withdraw(clients, balances, client, amount) {
+  for (let i = 0; i < clients.length; i++) {
+      if (clients[i] == client) {
+           balances =balances[i] - amount;
+          if (balances >= 0) {
+              return balances;
+          } else {
+              return -1;
+          }
+      }
+  }
+}

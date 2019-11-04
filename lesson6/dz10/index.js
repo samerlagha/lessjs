a3420e03109a44749b76501b42275ba43db4c29f
samerlagha <samer_la@mail.ru>
2019-11-04 08:44:31 +0200

    update

--- lesson6/dz10/index.js
+++ lesson6/dz10/index.js
@@ -1,11 +1,11 @@
 function withdraw(clients, balances, client, amount){
     let clientName=[];
     let  sum =[];
-     let withdrawMoney =[];
+     let withdrawMoney =0;
     for(let i =0 ; i <= clients.length-1;i++){
      for(let j=0 ; j <= balances.length-1;j++){
           if(withdrawMoney > balances) return -1;
-          balances[j] -=withdrawMoney[k];
+          balances[j] -=withdrawMoney;
      }
       return balances;
     }
